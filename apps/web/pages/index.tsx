5230fa0f14ac61a8aa1d5a41cb3f7584dddaf043
Firzan Armani <mrfirzanarmani@gmail.com>
2023-04-06 10:45:18 +0800

    feat(web): change demo image

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -7,6 +7,7 @@ import {
 import Link from "next/link";
 import { IconEyeOff } from "@tabler/icons-react";
 import Image from "next/image";
+import screenCap from "../assets/screenCap.gif";
 export default function Home() {
   return (
     <div className="bg-slate-900 h-full">
@@ -45,7 +46,7 @@ export default function Home() {
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           <div className="relative p-6 md:p-16">
             <div className="relative z-10 lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
-              <div className="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-8 lg:order-2">
+              <div className="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-7 lg:order-2">
                 <h2 className="text-2xl text-gray-800 font-bold sm:text-3xl dark:text-gray-200">
                   How MangaShow makes reading manga different
                 </h2>
@@ -118,9 +119,9 @@ export default function Home() {
                 <div className="relative">
                   <div>
                     <Image
+                      src={screenCap}
+                      alt="Demonstration"
                       className="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                      src="https://images.unsplash.com/photo-1605629921711-2f6b00c6bbf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
-                      alt="Image Description"
                     />
                   </div>
                 </div>
@@ -128,7 +129,7 @@ export default function Home() {
             </div>
 
             <div className="absolute inset-0 grid grid-cols-12 w-full h-full">
-              <div className="col-span-full lg:col-span-7 lg:col-start-6 bg-gray-100 w-full h-5/6 rounded-xl sm:h-3/4 lg:h-full dark:bg-white/[.075]"></div>
+              <div className="col-span-full lg:col-span-10 lg:col-start-3 bg-gray-100 w-full h-5/6 rounded-xl sm:h-3/4 lg:h-full dark:bg-white/[.075]"></div>
             </div>
           </div>
         </div>
